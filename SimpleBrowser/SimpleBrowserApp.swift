ac2a273ef693fb0be8a93ee8cfd0d9bb090c3cb1
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-10-22 12:58:05 -0400

    Initial Commit

---
+++ SimpleBrowser/SimpleBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBrowserApp.swift
+//  SimpleBrowser
+//
+//  Created by Dominic Teh on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleBrowserTests/SimpleBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBrowserTests.swift
+//  SimpleBrowserTests
+//
+//  Created by Dominic Teh on 22/10/24.
+//
+
+import Testing
+@testable import SimpleBrowser
+
+struct SimpleBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
